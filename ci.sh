d37d96fb5353b667e81179df804dd1ef343b03d3
Niels Koster <n.koster@portavita.eu>
2021-07-29 17:51:22 +0200

    Add service stop and start

--- ci.sh
+++ ci.sh
@@ -3,4 +3,6 @@
 while inotifywait -e close_write authServer.js
 do
     scp authServer.js palermo:apps/auth
+    ssh palermo 'systemctl --user stop auth_w3b_net.service'
+    ssh palermo 'systemctl --user start auth_w3b_net.service'
 done
